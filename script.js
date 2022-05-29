546f0515e0015bcd331ac4bde0d920cd07a64fa2
chirag_lala <chirag42001@gmail.com>
2022-05-29 16:59:57 +0530

    structure chaged and javascript code completed

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 let writing_area = document.getElementsByTagName("textarea")[0];
+let word_count = document.getElementById("word-count");
+let character_count = document.getElementById("character-count");
 
-let word_count = document.getElementsByTagName("span")[0];
-let character_count = document.getElementsByName("span")[1];
 
+writing_area.addEventListener('input',updateCounts);
 
-console.log(writing_area.value);
+function updateCounts(){
+    let current = writing_area.value.length;
+    character_count.innerHTML ="CHARACTER COUNT : "+current; 
+    let current_value = writing_area.value.split(" ");
+    let l = current_value.length;
+    let word = 0;
+    for(let i=0;i<l;i++)
+    {
+        if(current_value[i].length>0)
+        {
+            if(current_value[i]!=',' && current_value[i]!=';' && current_value[i]!='!' && current_value[i]!='?' &&
+            current_value[i]!='.')
+               word++;
+        }
+    }
+    
+    word_count.innerHTML = "WORD COUNT : "+word;
+}
