0d8f6e90f183ef8af6d232afa88e3ea3d8cc564d
chirag_lala <chirag42001@gmail.com>
2022-05-29 16:33:38 +0530

    page structure with styling complete javascript is left

---
+++ script.js
@@ -0,0 +1,7 @@
+let writing_area = document.getElementsByTagName("textarea")[0];
+
+let word_count = document.getElementsByTagName("span")[0];
+let character_count = document.getElementsByName("span")[1];
+
+
+console.log(writing_area.value);
